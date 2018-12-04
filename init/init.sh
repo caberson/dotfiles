31b14c1aa06c74e6df1dd94ed0f6b5fb92b3ce74
Caber Chu <caberchu@gmail.com>
2018-12-03 22:49:09 -0800

    Update several files with personal preference

--- init/init.sh
+++ init/init.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl https://github.com/git/git/blob/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
 git config --globa user.name "Caber Chu"
